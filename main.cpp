a303630d8abd929c6fc74d6ff40977bf0fccb663
Sergei <sgorcev84@mail.ru>
2023-11-16 05:16:22 +0300

    [+] реализация с тестами. Но надо ещё разделить на traits, чтобы не было всё в одном файле.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <print_ip.h>
 #include "version.h"
 
 using namespace std;
@@ -8,6 +9,15 @@ int main()
     using namespace std;
 
     cout << "My app version: " << PROJECT_VERSION << endl;
+
+    print_ip(int8_t{-1}) << std::endl;
+    print_ip(int16_t{0}) << std::endl;
+    print_ip(int32_t{2130706433}) << std::endl;
+    print_ip(int64_t{8875824491850138409}) << std::endl;
+    print_ip(std::string{"Hello, World!"}) << std::endl;
+    print_ip(std::vector<int>{100, 200, 300, 400}) << std::endl;
+    print_ip( std::list<short>{400, 300, 200, 100} ) << std::endl;
+    print_ip(std::make_tuple(123, 456, 789, 0)) << std::endl;
 
     return 0;
 }
